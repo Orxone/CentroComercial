a2677cbd541f6a6e76c9111a7ab738c435843e4c
Oriana <orianacsb@gmail.com>
2025-11-22 12:48:46 -0300

    Modo oscuro
    Paleta de colores, script para función de modo oscuro, arreglo de footer.

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,16 @@
 var theme = document.getElementById("mode-btn");
 
+if (localStorage.getItem("modo") === "oscuro"){
+    document.body.classList.add("dark-mode");
+}
+
 theme.onclick = function(){
     document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")){
+        localStorage.setItem("modo", "oscuro");
+    }
+
+    else{
+        localStorage.setItem("modo", "claro");
+    }
 }
