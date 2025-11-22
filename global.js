8f4c25859e678663053aa7391191defafc1b0bc2
Oriana <orianacsb@gmail.com>
2025-11-22 14:38:09 -0300

    Menú hamburguesa
    Menú hamburguesa desplegable para la barra de navegación.

--- global.js
+++ global.js
@@ -14,3 +14,15 @@ theme.onclick = function(){
         localStorage.setItem("modo", "claro");
     }
 }
+
+let burgerBtn = document.getElementById('burger-btn');
+let navbar = document.getElementById('nav-menu');
+
+burgerBtn.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+//Cierra el menú si el usuario scrollea 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
